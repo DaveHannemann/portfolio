12b1e49478f07a7c674dbffa7f33c183ae3cd6a6
Dave Hannemann <david.dikschat@web.de>
2025-10-03 19:32:43 +0200

    feat: added my skills section

--- src/app/main-content/skillset/skillset.component.ts
+++ src/app/main-content/skillset/skillset.component.ts
@@ -1,12 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-skillset',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './skillset.component.html',
-  styleUrl: './skillset.component.scss'
+  styleUrl: './skillset.component.scss',
 })
 export class SkillsetComponent {
+  skills = [
+    'assets/img/angular.png',
+    'assets/img/typescript.png',
+    'assets/img/javascript.png',
+    'assets/img/html.png',
+    'assets/img/css.png',
+    'assets/img/rest_api.png',
+    'assets/img/firebase.png',
+    'assets/img/git.png',
+    'assets/img/material_design.png',
+    'assets/img/scrum.png',
+  ];
 
+  learningSkills = [
+    { src: 'assets/img/angular_learn.png', label: 'Angular' },
+    { src: 'assets/img/typescript_learn.png', label: 'TypeScript' },
+    { src: 'assets/img/javascript_learn.png', label: 'JavaScript' },
+    { src: 'assets/img/html_learn.png', label: 'HTML' },
+    { src: 'assets/img/css_learn.png', label: 'CSS' },
+    { src: 'assets/img/firebase_learn.png', label: 'Firebase' },
+  ];
+
+  learningHover = false;
 }
