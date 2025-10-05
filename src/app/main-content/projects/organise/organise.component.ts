765917ff5c13ce309dcfe1f1caa6c780a47c2aa2
Dave Hannemann <david.dikschat@web.de>
2025-10-05 14:59:14 +0200

    feat: added new projects structure

---
+++ src/app/main-content/projects/organise/organise.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-organise',
+  standalone: true,
+  imports: [],
+  templateUrl: './organise.component.html',
+  styleUrl: './organise.component.scss'
+})
+export class OrganiseComponent {
+
+}
--- src/app/main-content/projects/projects.component.ts
+++ src/app/main-content/projects/projects.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { AboutComponent } from './about/about.component';
+import { OrganiseComponent } from './organise/organise.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { DisplayComponent } from './display/display.component';
+import { MenubarComponent } from './menubar/menubar.component';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [AboutComponent, OrganiseComponent, ExperienceComponent, DisplayComponent, MenubarComponent],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
