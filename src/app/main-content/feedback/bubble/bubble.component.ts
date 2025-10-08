947d8d10e44da2f1ee55c6bb336d5c47bd967cc6
Dave Hannemann <david.dikschat@web.de>
2025-10-08 12:44:37 +0200

    feat: added bubble for comments and structure

--- src/app/main-content/feedback/bubble/bubble.component.ts
+++ src/app/main-content/feedback/bubble/bubble.component.ts
@@ -1,12 +1,37 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-bubble',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './bubble.component.html',
-  styleUrl: './bubble.component.scss'
+  styleUrl: './bubble.component.scss',
 })
 export class BubbleComponent {
-feedbackHover = false;
+  hoverIndex: number | null = null;
+
+  feedbackData = [
+    {
+      name: 'Gustav G.',
+      class: 'Project',
+      project: 'Join',
+      message: 'Amazing',
+      linkedin: '',
+    },
+    {
+      name: 'Donald D.',
+      class: 'Project',
+      project: 'Pokédex',
+      message: 'Just wow!',
+      linkedin: '',
+    },
+    {
+      name: 'Peter P.',
+      class: 'Teammate',
+      project: 'Football',
+      message: 'Clean code structure and smart logic.',
+      linkedin: '',
+    },
+  ];
 }
