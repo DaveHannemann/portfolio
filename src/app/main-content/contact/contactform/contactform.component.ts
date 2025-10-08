7736eafba898f937e4b05e8a68e51c1def00c27e
Dave Hannemann <david.dikschat@web.de>
2025-10-08 19:41:17 +0200

    feat: add form and styling to contactform.component

--- src/app/main-content/contact/contactform/contactform.component.ts
+++ src/app/main-content/contact/contactform/contactform.component.ts
@@ -1,12 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contactform',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './contactform.component.html',
-  styleUrl: './contactform.component.scss'
+  styleUrl: './contactform.component.scss',
 })
 export class ContactformComponent {
 
+  policyHover = false;
+  policyClicked = false;
+  
+  formData = {
+    name: '',
+    email: '',
+    message: '',
+  };
 }
