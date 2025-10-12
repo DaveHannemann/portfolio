14e544f84cd3f449a13b25a31f893294087c564f
Dave Hannemann <david.dikschat@web.de>
2025-10-12 15:10:59 +0200

    feat: added footer and completed contactform

--- src/app/main-content/contact/contactform/contactform.component.ts
+++ src/app/main-content/contact/contactform/contactform.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
@@ -11,18 +12,52 @@ import { FormsModule, NgForm } from '@angular/forms';
 })
 export class ContactformComponent {
 
+  http = inject(HttpClient);
+
   policyHover = false;
   policyClicked = false;
-  
+  policyTouched = false;
+  formSubmitted = false;
+  mailTest = true;
+
   formData = {
     name: '',
     email: '',
     message: '',
   };
 
+  post = {
+    endPoint: 'https://deineDomain.de/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
+
   onSubmit(ngForm: NgForm) {
-    if (ngForm.valid && ngForm.submitted) {
-      console.log('Form Data:', this.formData); 
+    if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+      this.http.post(this.post.endPoint, this.post.body(this.formData))
+        .subscribe({
+          next: (response) => {
+
+            ngForm.resetForm();
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => console.info('send post complete'),
+        });
+    } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+
+      ngForm.resetForm();
+    }
   }
-}
+
+  togglePolicy() {
+    this.policyClicked = !this.policyClicked;
+    this.policyTouched = true;
+  }
 }
