ef6d0cd2c62e7535a6131fc00e7686812e4b087f
Dave Hannemann <davidhannemanndev@gmail.com>
2026-02-01 14:57:26 +0100

    fix: hydration deleted

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,6 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
 
 import { provideAnimations } from '@angular/platform-browser/animations';
@@ -16,7 +15,6 @@ export const appConfig: ApplicationConfig = {
         anchorScrolling: 'enabled'
       })
     ),
-    provideClientHydration(),
     provideHttpClient(),
     provideAnimations(),
   ],
